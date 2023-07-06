ee431b1a42e5fd592d18e84846e68777449f95e5
Ash Sendell-Price <36015770+ASendellPrice@users.noreply.github.com>
2023-07-06 13:27:43 +0100

    Create EToKi_assemble.sh

---
+++ EToKi_assemble.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+if [ -f EToKi_prepare_L1_SE.fastq.gz ]
+then
+  EToKi.py assemble --pe EToKi_prepare_L1_R1.fastq.gz,EToKi_prepare_L1_R2.fastq.gz --se EToKi_prepare_L1_SE.fastq.gz -k 15,30,50,70,90 -p EToKi_spades
+else
+  EToKi.py assemble --pe EToKi_prepare_L1_R1.fastq.gz,EToKi_prepare_L1_R2.fastq.gz -k 15,30,50,70,90 -p EToKi_spades
+fi
